ebbf42f02a2b834359db468ab3666e5ce053480b
David Luna de León <davidlunadeleon@gmail.com>
2022-05-16 11:22:36 -0500

    Fixes in Memory
    - Fix error where the data type MemoryAddress was not accepted as an
      index to retrieve an element in memory.
    - Fix error where __get_list_from_index did not return the string list.

--- src/memory.py
+++ src/memory.py
@@ -42,7 +42,7 @@ class Memory:
         elif index < self.strings.start_address:
             return self.ints
         else:
-            raise TypeError("Can't retrieve a list from an invalid index.")
+            return self.strings
 
     def __get_list_from_type(self, value: MemoryType) -> MemoryList:
         if isinstance(value, bool):
@@ -68,8 +68,12 @@ class Memory:
         else:
             raise TypeError("Can't retrieve a list from invalid type.")
 
-    def __getitem__(self, index: int) -> MemoryType:
-        return self.__get_list_from_index(index).values[index]
+    def __getitem__(self, index: MemoryAddress) -> MemoryType:
+        if isinstance(index, int):
+            l = self.__get_list_from_index(index)
+            return l.values[index - l.start_address]
+        else:
+            raise Exception("Can't access an invalid memory address.")
 
     def __setitem__(self, index: int, value: MemoryType) -> None:
         l = self.__get_list_from_index(index)
