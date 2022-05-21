bbb3a75cd2db16ee845cbb5d3fcd1693fc237baf
David Luna de León <davidlunadeleon@gmail.com>
2022-05-21 12:39:26 -0500

    Add new Scope and ScopeStack classes
    - Add class Scope to handle the var_table of each block of code, as well
      as its memory.
    - Add class ScopeStack to handle the stack of scope for multiple nested
      blocks of code.

---
+++ src/scope.py
@@ -0,0 +1,25 @@
+from .memory import Memory
+from .var_table import VarTable
+from .utils.types import TypeAddress
+from .utils.enums import ScopeTypes, Types
+
+
+class Scope:
+    var_table: VarTable
+    mem: Memory
+    type: ScopeTypes
+
+    def __init__(self, type: ScopeTypes, mem: Memory) -> None:
+        self.mem = mem
+        self.type = type
+        self.var_table = VarTable()
+
+    def has(self, var_name: str) -> bool:
+        return self.var_table.has(var_name)
+
+    def get(self, var_name: str) -> TypeAddress:
+        var_info = self.var_table.get(var_name)
+        return (var_info["type"], var_info["address"])
+
+    def add(self, var_name: str, var_type: Types):
+        self.var_table.add(var_name, var_type, self.mem.reserve(var_type))
---
+++ src/scope_stack.py
@@ -0,0 +1,31 @@
+from .scope import Scope
+from .utils.types import TypeAddress
+from .utils.enums import Types
+
+
+class ScopeStack:
+    scope_stack: list[Scope]
+
+    def __init__(self) -> None:
+        self.scope_stack = []
+
+    def push(self, scope: Scope) -> None:
+        self.scope_stack.append(scope)
+
+    def pop(self) -> Scope:
+        return self.scope_stack.pop()
+
+    def has_var(self, var_name: str) -> bool:
+        for scope in reversed(self.scope_stack):
+            if scope.has(var_name):
+                return True
+        return False
+
+    def get_var(self, var_name: str) -> TypeAddress:
+        for scope in reversed(self.scope_stack):
+            if scope.has(var_name):
+                return scope.get(var_name)
+        raise Exception(f"Could not find {var_name}.")
+
+    def add_var(self, var_name: str, var_type: Types) -> None:
+        self.scope_stack[-1].add(var_name, var_type)
