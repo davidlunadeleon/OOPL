00c6742982604726d22ebf5456b5465141994cfc
David Luna de León <davidlunadeleon@gmail.com>
2022-05-25 10:47:38 -0500

    Update Scope and ScopeStack
    - Update Scope to use the new VarInfo class.
    - Update ScopeStack to let the parser handle the creation of ArrayInfo.

--- src/scope.py
+++ src/scope.py
@@ -3,6 +3,7 @@ from .memory import Memory
 from .utils.enums import Types, ScopeTypes
 from .utils.types import TypeAddress
 from .var_table import VarTable
+from .var_info import VarInfo
 
 
 class Scope:
@@ -13,27 +14,15 @@ class Scope:
     def __init__(self, type: ScopeTypes, mem: Memory) -> None:
         self.mem = mem
         self.type = type
-        self.var_table = VarTable()
+        self.var_table = VarTable(self.mem)
 
     def has(self, var_name: str) -> bool:
         return self.var_table.has(var_name)
 
     def get(self, var_name: str) -> TypeAddress:
         var_info = self.var_table.get(var_name)
-        return (
-            var_info["type"],
-            var_info["address"],
-            var_info["name"],
-            var_info["array_info"],
-        )
+        return (var_info.type, var_info.address, var_info.name)
 
     def add(self, var_name: str, var_type: Types, array_info: ArrayInfo) -> TypeAddress:
-        var_info = self.var_table.add(
-            var_name, var_type, self.mem.reserve(var_type), array_info
-        )
-        return (
-            var_info["type"],
-            var_info["address"],
-            var_info["name"],
-            var_info["array_info"],
-        )
+        var_info = self.var_table.add(var_name, var_type, array_info)
+        return (var_info.type, var_info.address, var_info.name)
--- src/scope_stack.py
+++ src/scope_stack.py
@@ -1,4 +1,3 @@
-from typing import Type
 from .array_info import ArrayInfo
 from .scope import Scope
 from .utils.types import TypeAddress
@@ -33,33 +32,6 @@ class ScopeStack:
         self, var_name: str, var_type: Types, array_info: ArrayInfo
     ) -> TypeAddress:
         return self.scope_stack[-1].add(var_name, var_type, array_info)
-
-    def add_dimension_to_array(self, var_name: str, lim_s: int) -> TypeAddress:
-        old_arr = None
-        for scope in reversed(self.scope_stack):
-            if scope.has(var_name):
-                old_arr = scope.get(var_name)
-        if old_arr == None:
-            raise Exception(f"Could not find {var_name}.")
-        else:
-            if old_arr[-1] is None:
-                new_arr = ArrayInfo()
-                new_arr.add_dim(lim_s)
-                scope[var_name] = new_arr
-            elif old_arr[-1] is ArrayInfo:
-                old_arr[-1].add_dim(lim_s)
-                scope[var_name] = old_arr
-
-    def update_array_info(self, var_name: str) -> TypeAddress:
-        old_arr = None
-        for scope in reversed(self.scope_stack):
-            if scope.has(var_name):
-                old_arr = scope.get(var_name)
-        if old_arr == None:
-            raise Exception(f"Could not find {var_name}.")
-        else:
-            old_arr[-1].update_dims()
-            scope[var_name] = old_arr
 
     def is_in_loop(self) -> bool:
         for scope in reversed(self.scope_stack):
