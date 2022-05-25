7c323e1b796a22bdeee1cefcc0fd37ebf4c1d835
David Luna de León <davidlunadeleon@gmail.com>
2022-05-24 22:30:28 -0500

    Add handling of linear operations in VM
    - In VM.run handle the quadruples that do not require a context switch
      or any other complicated logic. Just handle linear instructions.

--- src/vm.py
+++ src/vm.py
@@ -1,7 +1,10 @@
+import sys
+
 from .func_dir import VMFuncDir
 from .memory import Memory
 from .quadruple_list import QuadrupleList
-from .utils.types import FunctionResources, Quadruple, MemoryType
+from .utils.types import FunctionResources, Quadruple, MemoryType, MemoryAddress
+from .utils.enums import Operations
 
 
 class VM:
@@ -17,6 +20,7 @@ class VM:
 
     def init_global_memory(self, global_resources: FunctionResources) -> None:
         self.global_memory = Memory(0, 1000, global_resources)
+        self.function_memory = Memory(4000, 1000, (20, 20, 20, 20))
 
     def add_function(self, name: str, start_quad: int, resources: FunctionResources):
         self.func_dir.add(name, start_quad, resources)
@@ -27,7 +31,66 @@ class VM:
     def set_global_variable(self, addr: int, val: MemoryType):
         self.global_memory[addr] = val
 
+    def __get_memory(self, address: MemoryAddress):
+        return (
+            self.function_memory
+            if address is not None and address >= 4000
+            else self.global_memory
+        )
+
     def run(self):
         quads_iter = iter(self.quads)
         for quad in quads_iter:
             print(quad)
+            op_code, addr1, addr2, addr3 = quad
+            mem1 = self.__get_memory(addr1)
+            mem2 = self.__get_memory(addr2)
+
+            if op_code is Operations.GOSUB:
+                continue
+
+            if isinstance(addr3, str) or addr3 is None:
+                addr3 = 0
+            mem3 = self.__get_memory(addr3)
+
+            if op_code is Operations.AND:
+                mem3[addr3] = mem1[addr1] and mem2[addr2]
+            elif op_code is Operations.ASSIGNOP:
+                mem3[addr3] = mem1[addr1]
+            elif op_code is Operations.DIFF:
+                mem3[addr3] = mem1[addr1] != mem2[addr2]
+            elif op_code is Operations.DIVIDES:
+                mem3[addr3] = mem1[addr1] / mem2[addr2]
+            elif op_code is Operations.ENDSUB:
+                pass
+            elif op_code is Operations.EQ:
+                mem3[addr3] = mem1[addr1] == mem2[addr2]
+            elif op_code is Operations.EQGT:
+                mem3[addr3] = mem1[addr1] >= mem2[addr2]
+            elif op_code is Operations.EQLT:
+                mem3[addr3] = mem1[addr1] <= mem2[addr2]
+            elif op_code is Operations.GOTOF:
+                pass
+            elif op_code is Operations.GOTOT:
+                pass
+            elif op_code is Operations.GT:
+                mem3[addr3] = mem1[addr1] > mem2[addr2]
+            elif op_code is Operations.LT:
+                mem3[addr3] = mem1[addr1] < mem2[addr2]
+            elif op_code is Operations.MINUS:
+                mem3[addr3] = mem1[addr1] - mem2[addr2]
+                pass
+            elif op_code is Operations.OR:
+                mem3[addr3] = mem1[addr1] or mem2[addr2]
+                pass
+            elif op_code is Operations.PARAM:
+                pass
+            elif op_code is Operations.PLUS:
+                mem3[addr3] = mem1[addr1] + mem2[addr2]
+                pass
+            elif op_code is Operations.PRINT:
+                print(mem1[addr1])
+            elif op_code is Operations.READ:
+                mem3[addr3] = sys.stdin.readline()
+            elif op_code is Operations.TIMES:
+                mem3[addr3] = mem1[addr1] * mem2[addr2]
