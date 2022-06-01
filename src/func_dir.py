42a06cd3505405a12cf5fec6dccb2331def78825
David Luna de León <davidlunadeleon@gmail.com>
2022-05-31 21:11:31 -0500

    Put FuncInfo classes in their own file
    - Take FuncInfo and the classes that inherit it, CFuncInfo and
      VMFuncInfo, and put them in their own file.

--- src/func_dir.py
+++ src/func_dir.py
@@ -9,62 +9,13 @@
 #
 #
 
-from typing import Generic, TypeVar
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
 
+from .func_info import CFuncInfo, VMFuncInfo
 from .scope import Scope
 from .utils.enums import Types
-from .utils.types import FunctionResources, MemoryAddress, ParamList
-
-
-class FuncInfo:
-    start_quad: int
-    resources: FunctionResources
-    name: str
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-
-
-class CFuncInfo(FuncInfo):
-    address: MemoryAddress
-    has_return: bool
-    is_body_defined: bool
-    param_list: ParamList
-    return_address: MemoryAddress | None
-    scope: Scope
-    type: Types
-
-    def __init__(
-        self,
-        name: str,
-        return_address: MemoryAddress | None,
-        scope: Scope,
-        type: Types,
-        address: MemoryAddress,
-    ) -> None:
-        super().__init__(name)
-        self.address = address
-        self.has_return = False
-        self.is_body_defined = False
-        self.param_list = []
-        self.resources = (0, 0, 0, 0, 0)
-        self.return_address = return_address
-        self.scope = scope
-        self.type = type
-
-
-class VMFuncInfo(FuncInfo):
-    def __init__(
-        self,
-        name: str,
-        start_quad: int,
-        resources: FunctionResources,
-    ) -> None:
-        super().__init__(name)
-        self.start_quad = start_quad
-        self.resources = resources
-
+from .utils.types import FunctionResources, MemoryAddress
 
 T = TypeVar("T", CFuncInfo, VMFuncInfo)
 
