5bc42b05b85dfe905f54c48e7752108e6b1f9b17
David Luna de León <davidlunadeleon@gmail.com>
2022-06-01 13:48:04 -0500

    Rename type alias FunctionResources to Resources

--- src/utils/types.py
+++ src/utils/types.py
@@ -9,8 +9,14 @@ TokenList = list[str]
 MemoryAddress: TypeAlias = int
 MemoryType = bool | float | int | str
 Quadruple = Tuple[Operations, MemoryAddress, MemoryAddress, MemoryAddress]
-FunctionResources = Tuple[int, int, int, int, int]
 TypeAddress = Tuple[str, MemoryAddress, str | None]
+
+NumBools: TypeAlias = int
+NumFloats: TypeAlias = int
+NumInts: TypeAlias = int
+NumStrings: TypeAlias = int
+NumPointers: TypeAlias = int
+Resources = Tuple[NumBools, NumFloats, NumInts, NumStrings, NumPointers]
 
 ParamName: TypeAlias = str
 ParamList: TypeAlias = list[Tuple[str, MemoryAddress, ParamName]]
