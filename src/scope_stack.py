23c3e04c5db218dd45fb96d359ee1244c21a9976
David Luna de León <davidlunadeleon@gmail.com>
2022-06-01 13:04:44 -0500

    Remove add_var from ScopeStack
    - Instead of calling add_var, it's better to call ScopeStack.top().add()

--- src/scope_stack.py
+++ src/scope_stack.py
@@ -1,10 +1,6 @@
-from typing import Optional
-
-from .array_info import ArrayInfo
 from .containers.stack import Stack
 from .scope import Scope
 from .utils.enums import ScopeTypes
-from .utils.types import TypeAddress
 from .var_info import VarInfo
 
 
@@ -23,11 +19,6 @@ class ScopeStack(Stack[Scope]):
             if scope.has(var_name):
                 return scope.get(var_name)
         raise Exception(f"Could not find {var_name}.")
-
-    def add_var(
-        self, var_name: str, var_type: str, array_info: Optional[ArrayInfo] = None
-    ) -> TypeAddress:
-        return self.top().add(var_name, var_type, array_info)
 
     def is_in_loop(self) -> bool:
         for scope in reversed(self.stack):
