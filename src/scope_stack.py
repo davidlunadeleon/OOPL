bb624ea2df2ee18a15067311a0bae4b7e891ffef
David Luna de León <davidlunadeleon@gmail.com>
2022-06-01 12:51:59 -0500

    Update ScopeStack to inherit Stack

--- src/scope_stack.py
+++ src/scope_stack.py
@@ -1,35 +1,25 @@
 from typing import Optional
 
 from .array_info import ArrayInfo
+from .containers.stack import Stack
 from .scope import Scope
-from .utils.types import TypeAddress
 from .utils.enums import ScopeTypes
+from .utils.types import TypeAddress
 from .var_info import VarInfo
 
 
-class ScopeStack:
-    scope_stack: list[Scope]
-
+class ScopeStack(Stack[Scope]):
     def __init__(self) -> None:
-        self.scope_stack = []
-
-    def push(self, scope: Scope) -> None:
-        self.scope_stack.append(scope)
-
-    def pop(self) -> Scope:
-        return self.scope_stack.pop()
-
-    def top(self) -> Scope:
-        return self.scope_stack[-1]
+        super().__init__()
 
     def has_var(self, var_name: str) -> bool:
-        for scope in reversed(self.scope_stack):
+        for scope in reversed(self.stack):
             if scope.has(var_name):
                 return True
         return False
 
     def get_var(self, var_name: str) -> VarInfo:
-        for scope in reversed(self.scope_stack):
+        for scope in reversed(self.stack):
             if scope.has(var_name):
                 return scope.get(var_name)
         raise Exception(f"Could not find {var_name}.")
@@ -37,10 +27,10 @@ class ScopeStack:
     def add_var(
         self, var_name: str, var_type: str, array_info: Optional[ArrayInfo] = None
     ) -> TypeAddress:
-        return self.scope_stack[-1].add(var_name, var_type, array_info)
+        return self.top().add(var_name, var_type, array_info)
 
     def is_in_loop(self) -> bool:
-        for scope in reversed(self.scope_stack):
+        for scope in reversed(self.stack):
             if scope.type is ScopeTypes.LOOP:
                 return True
         return False
