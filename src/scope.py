1b2c3940ede78a58043e740ff861d106dfef54c2
David Luna de León <davidlunadeleon@gmail.com>
2022-06-01 16:54:26 -0500

    Update Scope to handle classes
    - Update Scope to optionally take the parameter var_table during
      initialization. Useful for classes.
    - Update add method to not reserve memory in the case of a class
      variable.

--- src/scope.py
+++ src/scope.py
@@ -13,10 +13,12 @@ class Scope:
     type: ScopeTypes
     var_table: VarTable
 
-    def __init__(self, type: ScopeTypes, mem: Memory) -> None:
+    def __init__(
+        self, type: ScopeTypes, mem: Memory, var_table: Optional[VarTable] = None
+    ) -> None:
         self.mem = mem
         self.type = type
-        self.var_table = VarTable()
+        self.var_table = var_table or VarTable()
 
     def has(self, var_name: str) -> bool:
         return self.var_table.has(var_name)
@@ -27,9 +29,12 @@ class Scope:
     def add(
         self, var_name: str, var_type: str, array_info: Optional[ArrayInfo] = None
     ) -> TypeAddress:
-        if array_info is None:
-            address = self.mem.reserve(var_type)
+        if self.type is not ScopeTypes.CLASS:
+            if array_info is None:
+                address = self.mem.reserve(var_type)
+            else:
+                address = self.mem.reserve(var_type, array_info.size)
         else:
-            address = self.mem.reserve(var_type, array_info.size)
+            address = 0
         var_info = self.var_table.add(var_name, var_type, address, array_info)
         return (var_info.type, var_info.address, var_info.name)
