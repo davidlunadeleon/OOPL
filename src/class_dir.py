a46bcf203b04d4e621f882077790a734016ae1b3
ym2 <yulisa.med2@gmail.com>
2022-06-02 18:52:13 -0500

    Documenting class_dir

--- src/class_dir.py
+++ src/class_dir.py
@@ -7,12 +7,21 @@ class ClassDir(Dir[ClassInfo]):
         super().__init__()
 
     def add(self, name: str):
+        """
+        Inserting a new class into the directory.
+
+        Arguments:
+        name: str -- Name of the class to be added.
+        """
         if name in self.dir:
             raise Exception(f"The class {name} was already defined.")
         else:
             self.dir[name] = ClassInfo(name)
 
     def print(self, verbose: bool) -> None:
+        """
+        Print nformation of the directory depending on verbose flag value.
+        """
         for value in self.values():
             if verbose:
                 print(f"# {value.name}")
