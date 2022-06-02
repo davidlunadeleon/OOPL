a56e8a8e23d223f6447323ba7e55162392f62426
ym2 <yulisa.med2@gmail.com>
2022-06-02 11:03:51 -0500

    Update class_dir.py

--- src/class_dir.py
+++ src/class_dir.py
@@ -1,9 +1,7 @@
 from src.attribute_info import AttributeInfo
-# from src.method_info import MethodInfo
 
 class classInfo:
     attr_dir: dict[str, AttributeInfo]
-    # met_dir: dict[str, MethodInfo]
 
 class ClassDir():
     class_dir: dict[str, classInfo]
