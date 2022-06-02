f5d0ff4ab5f6abea05ad983b1ec51b47d9c1d229
ym2 <yulisa.med2@gmail.com>
2022-06-02 11:13:04 -0500

    Has and get attribute from class dir

--- src/class_dir.py
+++ src/class_dir.py
@@ -1,19 +1,30 @@
 from src.attribute_info import AttributeInfo
 
-class classInfo:
-    attr_dir: dict[str, AttributeInfo]
-
 class ClassDir():
-    class_dir: dict[str, classInfo]
+    attr_dir: dict[str, AttributeInfo]
 
     def __init__(self):
         self.class_dir = {}
+
     
-    def has_var(self, name: str) -> bool:
+    def get_attr(self, name: str) -> AttributeInfo:
+        """
+        Return attribute info corresponding to attribute with that name.
+
+        Arguments:
+        name: str -- Name of the attribute.
+        """
+        if(attr_info := self.attr_dir.get(name)) is not None:
+            return attr_info
+        else:
+            raise Exception(f"Couldn't retrieve the information of attribute {name}.")
+    
+    def has_attr(self, name: str) -> bool:
         """
         Check whether a variable is contained in the directory.
 
         Arguments:
-        name: str -- Name of the variable.
+        name: str -- Name of the attribute.
         """
-        return False is self.
+        return False if self.attr_dir.get_attr(name) is None else True
+    
