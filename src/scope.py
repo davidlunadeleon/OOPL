577d159946861f5269bc2b99431aceefbc49118c
ym2 <yulisa.med2@gmail.com>
2022-06-03 05:07:23 -0500

    Documented scope

--- src/scope.py
+++ src/scope.py
@@ -21,14 +21,34 @@ class Scope:
         self.var_table = var_table or VarTable()
 
     def has(self, var_name: str) -> bool:
+        """
+        Check if a specific variable was defined in this scope.
+
+        Arguments:
+        var_name: str -- Name of the variable.
+        """
         return self.var_table.has(var_name)
 
     def get(self, var_name: str) -> VarInfo:
+        """
+        Get the information of a variable.
+
+        Arguments:
+        var_name: str -- Name of the variable.
+        """
         return self.var_table.get(var_name)
 
     def add(
         self, var_name: str, var_type: str, array_info: Optional[ArrayInfo] = None
     ) -> TypeAddress:
+        """
+        Reserve the space for the defined variable found in this scope and return its information.
+
+        Arguments:
+        var_name: str -- Name of the variable.
+        var_type: str -- Type of the variable.
+        array_info: Optional[ArrayInfo] -- Information about the dimensions and size of an array.
+        """
         if self.type is not ScopeTypes.CLASS and var_type in MemoryTypeNames:
             if array_info is None:
                 address = self.mem.reserve(var_type)
