ae1f547328bae061d5f2d5e292cbc9625bf4f51a
ym2 <yulisa.med2@gmail.com>
2022-06-03 05:04:06 -0500

    Documented memory

--- src/memory.py
+++ src/memory.py
@@ -8,7 +8,7 @@ T = TypeVar(
     "T", None, bool | None, float | None, int | None, str | None, MemoryAddress | None
 )
 
-
+# Contains a collection of values for a specific type
 class MemoryList(Generic[T]):
     values: list[T]
     start_address: int
@@ -37,6 +37,10 @@ class Memory:
         chunk_size: int = 1000,
         resources: Optional[Resources] = None,
     ) -> None:
+        """
+        Basic constructor that either creates memory lists with a defined size for compilation or tailors 
+        a new memory list to the specific amount of resources needed during runtime.
+        """
         self.chunk_size = chunk_size
         if resources is None:
             self.bools = MemoryList(base_address)
@@ -52,6 +56,12 @@ class Memory:
             self.ptrs = MemoryList(base_address + self.chunk_size * 4, resources[4])
 
     def __get_list_from_index(self, index: int) -> MemoryList:
+        """
+        Get a memory list of a specific type.
+
+        Arguments:
+        index: int -- Address that is in the range of a specific memory list.
+        """
         if index < self.floats.start_address:
             return self.bools
         elif index < self.ints.start_address:
@@ -64,6 +74,12 @@ class Memory:
             return self.ptrs
 
     def __get_list_from_t(self, t: str) -> MemoryList:
+        """
+        Get a memory list of a specific type.
+
+        Arguments:
+        t: str -- Type wanted.
+        """
         match t:
             case Types.BOOL.value:
                 return self.bools
@@ -81,10 +97,23 @@ class Memory:
                 raise TypeError("Can't retrieve a list from invalid type.")
 
     def __getitem__(self, index: MemoryAddress) -> MemoryType:
+        """
+        Get the type of an entry of a memory list.
+
+        Arguments:
+        index: MemoryAddress -- Address where the entry is stored.
+        """
         l = self.__get_list_from_index(index)
         return l.values[index - l.start_address]
 
     def __setitem__(self, index: int, value: MemoryType) -> None:
+        """
+        Set the content of an entry on a memory list.
+
+        Arguments:
+        index: int -- Address where the entry is.
+        value: MemoryType -- Type of the value stored.
+        """
         l = self.__get_list_from_index(index)
 
         match l:
@@ -104,6 +133,13 @@ class Memory:
         l.values[index - l.start_address] = value
 
     def __append(self, l: MemoryList, value: MemoryType | None) -> MemoryAddress:
+        """
+        Insert a new entry on a memory list.
+
+        Arguments:
+        l: MemoryList -- List where the entry will be stored.
+        value: MemoryType -- Type of the value stored.
+        """
         l.values.append(value)
         l_len = len(l.values) - 1
         if l_len > self.chunk_size:
@@ -112,20 +148,48 @@ class Memory:
             return l_len + l.start_address
 
     def save_ptr(self, index: int, value: int) -> None:
+        """
+        Save the instruction pointer our memory.
+
+        Arguments:
+        index: int -- Address where the entry should be.
+        value: int -- Last quadruple executed.
+        """
         l = self.ptrs
         l.values[index - l.start_address] = value
 
     def append(self, type: str, value: MemoryType) -> MemoryAddress:
+        """
+        Insert a new value to a memory list from our memory.
+
+        Arguments:
+        type:str -- Type of the value to be stored.
+        value: MemoryType: Type of the value to be stored.
+        """
         l = self.__get_list_from_t(type)
         return self.__append(l, value)
 
     def reserve(self, t: str, size: int = 1) -> MemoryAddress:
+        """
+        Reserve a space of memory for future operations corresponding to the correct type.
+
+        Arguments:
+        t: str -- Type of variable to be stored.
+        size: int = 1 -- Size of the memory space needed, can vary depending on simple vs array variable.
+        """
         l = self.__get_list_from_t(t)
         initial_address = self.__append(l, None)
         [self.__append(l, None) for _ in range(size - 1)]
         return initial_address
 
     def find(self, type: str, value: MemoryType) -> MemoryAddress | None:
+        """
+        Get address for a specific value type.
+
+        Arguments:
+        index: int -- Address where the entry will be stored.
+        value: MemoryType -- Type of the value stored.
+        """
         l = self.__get_list_from_t(type)
         try:
             return l.values.index(value) + l.start_address
@@ -133,12 +197,18 @@ class Memory:
             return None
 
     def clear(self) -> None:
+        """
+        Delete all content of memory lists.
+        """
         self.bools.values.clear()
         self.floats.values.clear()
         self.ints.values.clear()
         self.strings.values.clear()
 
     def describe_resources(self) -> Resources:
+        """
+        Get amount of resources needed per type.
+        """
         return (
             len(self.bools.values),
             len(self.floats.values),
@@ -148,10 +218,19 @@ class Memory:
         )
 
     def is_ptr(self, address: MemoryAddress) -> bool:
+        """
+        Check if the content of a memory address is a pointer.
+
+        Arguments:
+        address: MemoryAddress -- Address where the entry is stored.
+        """
         l = self.__get_list_from_index(address)
         return l == self.ptrs
 
     def print(self, verbose: bool, comment: bool = False):
+        """
+        Printing to show information about the memory lists depending on verbose value.
+        """
         if verbose:
             print("# bools")
         for index, item in enumerate(self.bools.values):
