74874437731052bd981ac388a69c67fdda398c04
ym2 <yulisa.med2@gmail.com>
2022-06-03 05:10:31 -0500

    Documented scope_stack

--- src/scope_stack.py
+++ src/scope_stack.py
@@ -9,24 +9,42 @@ class ScopeStack(Stack[Scope]):
         super().__init__()
 
     def has_var(self, var_name: str) -> bool:
+        """
+        Check if a specific variable was defined in any scope.
+
+        Arguments:
+        var_name: str -- Name of the variable.
+        """
         for scope in reversed(self.stack):
             if scope.has(var_name):
                 return True
         return False
 
     def get_var(self, var_name: str) -> VarInfo:
+        """
+        Get the information of a variable searching from top to bottom scopes.
+
+        Arguments:
+        var_name: str -- Name of the variable.
+        """
         for scope in reversed(self.stack):
             if scope.has(var_name):
                 return scope.get(var_name)
         raise Exception(f"Could not find {var_name}.")
 
     def is_in_loop(self) -> bool:
+        """
+        Check if the current or any of the previous contexts are all loops, useful to check for breaks.
+        """
         for scope in reversed(self.stack):
             if scope.type is ScopeTypes.LOOP:
                 return True
         return False
 
     def is_in_class(self) -> bool:
+        """
+        Check if the current or any of the previous contexts are all loops, useful to check for this.
+        """
         for scope in reversed(self.stack):
             if scope.type is ScopeTypes.CLASS:
                 return True
